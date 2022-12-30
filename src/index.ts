9b8797d392278b34357e539608a18b36a86b5a28
queuebit <queuebit10648@gmail.com>
2022-12-30 16:12:14 -0500

    Make test pass.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-export function streakCounter(storage: Storage, date: Date) {
-  return {};
+interface Streak {
+  currentCount: number;
+  startDate: string;
+  lastLoginDate: string;
+}
+
+export function streakCounter(storage: Storage, date: Date): Streak {
+  return {
+    currentCount: 0,
+    startDate: "12/1/2021",
+    lastLoginDate: "12/1/2021",
+  };
 }
