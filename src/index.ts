b81616888f892659594482512ee824a7bb480665
queuebit <queuebit10648@gmail.com>
2022-12-30 16:36:13 -0500

    Make test pass by retrieving existing streak.

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,17 @@ interface Streak {
 const KEY = "streak";
 
 export function streakCounter(storage: Storage, date: Date): Streak {
+  const streakInLocalStorage = storage.getItem(KEY);
+
+  if (streakInLocalStorage) {
+    try {
+      const streak = JSON.parse(streakInLocalStorage || "");
+      return streak;
+    } catch (error) {
+      console.error("Failed to parse streak from localStorage");
+    }
+  }
+
   const streak = {
     currentCount: 1,
     startDate: formattedDate(date),
