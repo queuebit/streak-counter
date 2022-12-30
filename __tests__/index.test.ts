007b0acac1f3ca28bcf3bca31d59ef593399762c
queuebit <queuebit10648@gmail.com>
2022-12-30 15:57:10 -0500

    Add failing test for streakCounter.

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,5 +1,27 @@
+import { JSDOM } from "jsdom";
+import { streakCounter } from "../src/index";
+
 describe("basic test", () => {
   it("should pass", () => {
     expect(true).toBe(true);
+  });
+});
+
+describe("streakCounter", () => {
+  let mockLocalStorage: Storage;
+
+  beforeEach(() => {
+    const mockJSDom = new JSDOM("", { url: "https://localhost" });
+
+    mockLocalStorage = mockJSDom.window.localStorage;
+  });
+
+  it("should return a streak object with currentCount, startDate and lastLoginDate", () => {
+    const date = new Date();
+    const streak = streakCounter(mockLocalStorage, date);
+
+    expect(streak.hasOwnProperty("currentCount")).toBe(true);
+    expect(streak.hasOwnProperty("startDate")).toBe(true);
+    expect(streak.hasOwnProperty("lastLoginDate")).toBe(true);
   });
 });
