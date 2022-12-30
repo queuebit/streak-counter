f33e73eb5d45b90d1919a1f272092ad68ab3f997
queuebit <queuebit10648@gmail.com>
2022-12-30 16:31:05 -0500

    Added failing test for using existing streak.

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -47,4 +47,38 @@ describe("streakCounter", () => {
     const streakAsString = mockLocalStorage.getItem(key);
     expect(streakAsString).not.toBeNull();
   });
+
+  describe("with a pre-populated streak", () => {
+    let mockLocalStorage: Storage;
+    const MOCKDATE = "12/12/2021";
+
+    beforeEach(() => {
+      const mockJSDom = new JSDOM("", { url: "https://localhost" });
+
+      mockLocalStorage = mockJSDom.window.localStorage;
+
+      // use date in past so it's always the same
+      const date = new Date(MOCKDATE);
+
+      const streak = {
+        currentCount: 1,
+        startDate: formattedDate(date),
+        lastLoginDate: formattedDate(date),
+      };
+
+      mockLocalStorage.setItem("streak", JSON.stringify(streak));
+    });
+
+    afterEach(() => {
+      mockLocalStorage.clear();
+    });
+
+    it("should return the streak from localStorage", () => {
+      const date = new Date();
+      const streak = streakCounter(mockLocalStorage, date);
+
+      // Should match the dates used to setup the tests
+      expect(streak.startDate).toBe(MOCKDATE);
+    });
+  });
 });
